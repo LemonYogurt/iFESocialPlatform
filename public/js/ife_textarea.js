b836378ae4b797760e23d6392f88bc8e8987c594
liuchao <liuchao@goyoo.com>
2016-04-14 19:10:32 +0800

    save

--- public/js/ife_textarea.js
+++ public/js/ife_textarea.js
@@ -67,7 +67,7 @@ publishContent.prototype.publicBtn = function () {
 				}
 				
 				if(self.iNum % 2){
-					$('#J_publicTextarea').css('background', '#f99');
+					$('#J_publicTextarea').css('background', 'rgba(255, 144, 144, 0.5)');
 				} else{
 					$('#J_publicTextarea').css('background', '');	
 				}
@@ -96,7 +96,56 @@ publishContent.prototype.faceBtn = function () {
 	});
 }
 
+publishContent.prototype.uploadPicBtn = function () {
+	$('#J_content_fileUpload').click(function () {
+		var _$elem = $('.ife_fileUploadTooltip');
+		var flag = _$elem.is(':hidden');
+		if (flag) {
+			_$elem.show(500);
+		} else {
+			_$elem.hide(500);
+		}
+	});
+
+	$(document).click(function (e) {
+		if ($(e.target).closest('.ife_fileUploadTooltip').length != 1 && $(e.target).closest('#J_content_fileUpload').length != 1) {
+			$('.ife_fileUploadTooltip').hide(500);
+		}
+	});
+	$('#J_closePublishPic').click(function () {
+		$('.ife_fileUploadTooltip').hide(500);
+	});
+};
+
+publishContent.prototype.uploadPic = function () {
+	$('#J_fileUploadPic').click(function () {
+		if($('#J_publishPic').length > 0) {
+			$('#J_publishPic').change(function (e) {
+				var pic = e.target.files[0];
+				var imgRep = /^image\/(jpeg|jpg|gif|png|bmp$)/i;
+				if (imgRep.test(pic.type)) {
+					var fr = new FileReader();
+					fr.readAsDataURL(pic);
+					// 首先进行本地预览，本地预览成功后，上传到服务器
+					fr.onload = function () {
+						$('#J_fileUploadPic img').attr('src', this.result);
+						$('#J_fileUploadPic img').show();
+
+						window.message['success']('图片本地预览成功');
+						$('.ife_fileUploadControl p').html('已上传图片');
+						$('#J_publishPic').remove();
+					};
+				}
+			});
+			$('#J_publishPic').click();
+		}
+	});
+};
+
+
 var pc = new publishContent();
 pc.initTextArea();
 pc.publicBtn();
 pc.faceBtn();
+pc.uploadPicBtn();
+pc.uploadPic();
